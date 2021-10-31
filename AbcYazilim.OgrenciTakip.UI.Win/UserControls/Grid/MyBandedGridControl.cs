0c1357ab76edce09c029a94c633f51a67a1c4001
DESKTOP-NACS1KS\asus <zeki.sert@prestijyazilim.com.tr>
2021-10-31 10:56:06 +0300

    GridControl Eklendi.

---
+++ AbcYazilim.OgrenciTakip.UI.Win/UserControls/Grid/MyBandedGridControl.cs
@@ -0,0 +1,15 @@
+﻿using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Views.BandedGrid;
+
+namespace AbcYazilim.OgrenciTakip.UI.Win.UserControls.Grid
+{
+    public class MyBandedGridControl : GridControl
+    {
+        
+    }
+
+    public class MyBandedGridView : BandedGridView
+    {
+
+    }
+}
