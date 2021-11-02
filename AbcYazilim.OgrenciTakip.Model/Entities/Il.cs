fd76000bd833d80d4c4b859d5eea67bc81f7ad92
DESKTOP-NACS1KS\asus <zeki.sert@prestijyazilim.com.tr>
2021-11-02 22:24:31 +0300

    Okul Il Ilce Entityleri Eklendi Iliskilendirme Yapıldı.

---
+++ AbcYazilim.OgrenciTakip.Model/Entities/Il.cs
@@ -0,0 +1,10 @@
+﻿using AbcYazilim.OgrenciTakip.Model.Entities.Base;
+
+namespace AbcYazilim.OgrenciTakip.Model.Entities
+{
+    public class Il : BaseEntityDurum
+    {
+        public string IlAdi { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
