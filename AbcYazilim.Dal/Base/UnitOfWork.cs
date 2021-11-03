2e2a49ea313847a4d8b973d6d09271c21202ec07
zekisert <zsert1963@gmail.com>
2021-11-04 02:10:52 +0300

    Hata Mesajları UnitOfWork.cs Eklendi.

--- AbcYazilim.Dal/Base/UnitOfWork.cs
+++ AbcYazilim.Dal/Base/UnitOfWork.cs
@@ -3,12 +3,15 @@ using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Data.SqlClient;
 using AbcYazilim.Dal.Interfaces;
+using AbcYazilim.OgrenciTakip.Common.Message;
 
 namespace AbcYazilim.Dal.Base
 {
     public class UnitOfWork<T> : IUnitOfWork<T> where T : class
     {
-        private readonly DbContext _context;
+        #region Variables
+        private readonly DbContext _context; 
+        #endregion
 
         public UnitOfWork(DbContext context)
         {
@@ -28,35 +31,69 @@ namespace AbcYazilim.Dal.Base
             }
             catch (DbUpdateException ex)
             {
-                var sqlEx = (SqlException)ex.InnerException?.InnerException;
+                var sqlEx = (SqlException) ex.InnerException?.InnerException;
                 if (sqlEx == null)
                 {
-
+                    Messages.HataMesaji(ex.Message);
+                    return false;
                 }
+
+                switch (sqlEx.Number)
+                {
+                    case 208:
+                        Messages.HataMesaji("İşlem Yapmak İstediğiniz Tablo Veritabanında Bulunamadı.");
+                        break;
+                    case 547:
+                        Messages.HataMesaji("Seçilen Kartın İşlem Görmüş Hareketleri Var Kart Silinemez.");
+                        break;
+                    case 2601:
+                    case 2627:
+                        Messages.HataMesaji("Girmiş Olduğunuz Id Daha Önce Kullanılmıştır.");
+                        break;
+                    case 4060:
+                        Messages.HataMesaji("İşlem Yapmak İstediğiniz Veritabanı Sunucuda Bulunamadı.");
+                        break;
+                    case 18456:
+                        Messages.HataMesaji("Server'a Bağlanılmak İstenilen Kullanıcı Adı ve Şifre Hatalıdır.");
+                        break;
+                    default:
+                        Messages.HataMesaji(sqlEx.Message);
+                        break;
+                }
+
+                return false;
             }
+            catch (Exception ex)
+            {
+                Messages.HataMesaji(ex.Message);
+                return false;
+            }
+
+            return true;
         }
 
+        #region Dispose
         protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
             {
                 if (disposing)
                 {
-                    // TODO: dispose managed state (managed objects)
+                    _context.Dispose();
                 }
 
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
+
                 disposedValue = true;
             }
         }
 
-        
+
         public void Dispose()
         {
-            
+
             Dispose(disposing: true);
-            System.GC.SuppressFinalize(this);
-        }
+            GC.SuppressFinalize(this);
+        } 
+        #endregion
     }
 }
