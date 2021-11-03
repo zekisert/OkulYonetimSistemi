0c7414a659577fd7375824071c256f9036524245
zekisert <zsert1963@gmail.com>
2021-11-04 01:06:55 +0300

    UnitOfWork.cs Eklendi.

---
+++ AbcYazilim.Dal/Base/UnitOfWork.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using AbcYazilim.Dal.Interfaces;
+
+namespace AbcYazilim.Dal.Base
+{
+    public class UnitOfWork<T> : IUnitOfWork<T> where T : class
+    {
+        private readonly DbContext _context;
+
+        public UnitOfWork(DbContext context)
+        {
+            if (context == null) return;
+            _context = context;
+        }
+
+        private bool disposedValue;
+
+        public IRepository<T> Rep => new Repository<T>(_context);
+
+        public bool Save()
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                var sqlEx = (SqlException)ex.InnerException?.InnerException;
+                if (sqlEx == null)
+                {
+
+                }
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        
+        public void Dispose()
+        {
+            
+            Dispose(disposing: true);
+            System.GC.SuppressFinalize(this);
+        }
+    }
+}
