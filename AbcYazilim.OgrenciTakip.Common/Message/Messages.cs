25a2b36b04e893a5bfff38cb5e17032f3c104208
zekisert <zsert1963@gmail.com>
2021-11-04 01:13:30 +0300

    Common Katmanına HataMesaj Metodu Eklendi.

---
+++ AbcYazilim.OgrenciTakip.Common/Message/Messages.cs
@@ -0,0 +1,13 @@
+﻿using DevExpress.XtraEditors;
+using System.Windows.Forms;
+
+namespace AbcYazilim.OgrenciTakip.Common.Message
+{
+    public class Messages
+    {
+        public static void HataMesaji(string hataMesaji)
+        {
+            XtraMessageBox.Show(hataMesaji, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
